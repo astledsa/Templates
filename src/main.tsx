c43b68490595b36a0e891b1b0d9bd8dd05e69ea9
Astle Dsa <astledsa@gmail.com>
2025-08-16 15:11:23 +0530

    convex setup + auth

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { StrictMode } from "react";
 import App from "./App";
 import "./index.css";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 createRoot(document.getElementById("root")!).render(
         <StrictMode>
                 <ConvexProvider client={convex}>
-                        <App />
+                        <ConvexAuthProvider client={convex}>
+                                <App />
+                        </ConvexAuthProvider>
                 </ConvexProvider>
         </StrictMode>,
 );
